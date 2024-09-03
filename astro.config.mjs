713b78da11c9231f988b2a5d4532ddfddc84ef66
Greg P <88697977+zvxvx@users.noreply.github.com>
2024-09-03 12:51:22 -0700

    Basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import robotsTxt from 'astro-robots-txt';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt(), sitemap()]
 });
