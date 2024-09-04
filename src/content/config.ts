8d2f7709eb1b81243569ac02df669a4fc6165862
Greg P <88697977+zvxvx@users.noreply.github.com>
2024-09-04 11:43:13 -0700

    Set category types

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,37 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: 'content', 
+  type: "content",
   schema: z.object({
     title: z.string(),
     image: z.string().optional(),
     date: z.date(),
-    description: z.string().optional()
+    description: z.string().optional(),
   }),
+});
+const bwCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      slug: z.string(),
+      image: image(),
+      date: z.date(),
+    }),
+});
+const colorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      slug: z.string(),
+      image: image(),
+      date: z.date(),
+    }),
 });
 
 export const collections = {
-  'blog': blogCollection,
+  blog: blogCollection,
+  bw: bwCollection,
+  color: colorCollection,
 };
