3bb5482d5e41bd206b9160a6ab8cc0900de5a02d
Greg P <88697977+zvxvx@users.noreply.github.com>
2024-09-05 11:53:22 -0700

    Set sitemap url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import robotsTxt from 'astro-robots-txt';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap()]
+  site: "https://astro-photo-gallery.netlify.app", //change to your site
+  integrations: [tailwind(), robotsTxt(), sitemap()],
 });
