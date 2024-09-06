139ecbd8d6b021f31ed7b9c900c61c0c40645718
Greg P <88697977+zvxvx@users.noreply.github.com>
2024-09-06 15:43:33 -0700

    Update layout styling

--- src/styleVars.ts
+++ src/styleVars.ts
@@ -1,2 +1,2 @@
 export const STYLE =
-  "prose prose-headings:m-0 dark:text-white bg-slate-200 dark:bg-slate-700 dark:prose-strong:text-white prose-headings:dark:text-white prose-hr:border-black dark:prose-hr:border-white prose-a:dark:text-white mx-auto prose-a:no-underline prose-hr:my-2 marker:text-black marker:dark:text-white px-2 prose-ul:p-0 prose-ul:m-0 prose-li:list-none";
+  "prose prose-headings:m-0 dark:text-white bg-slate-200 dark:bg-slate-700 dark:prose-strong:text-white prose-headings:dark:text-white prose-hr:border-black dark:prose-hr:border-white prose-a:dark:text-white mx-auto prose-a:no-underline prose-hr:my-2 marker:text-black marker:dark:text-white px-2 prose-ul:p-0 prose-ul:m-0 prose-li:list-none prose-li:p-0";
