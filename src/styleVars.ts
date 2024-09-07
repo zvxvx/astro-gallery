12f9c183f996e2b54e43e3af0ea702adbd6b01c6
Greg <88697977+zvxvx@users.noreply.github.com>
2024-09-06 20:00:43 -0700

    Simplify styling between light and dark

--- src/styleVars.ts
+++ src/styleVars.ts
@@ -1,2 +1,2 @@
 export const STYLE =
-  "mx-auto sm:px-0 px-2 prose dark:prose-invert prose-headings:my-2 bg-zinc-300 dark:bg-zinc-800 prose-hr:border-black dark:prose-hr:border-white prose-a:no-underline prose-hr:my-2";
+  "mx-auto sm:px-0 px-2 prose dark:prose-invert prose-headings:my-2 bg-zinc-200/50 dark:bg-zinc-900/95 prose-hr:border-black dark:prose-hr:border-white prose-a:no-underline prose-hr:my-2";
