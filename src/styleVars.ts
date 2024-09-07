09209d7f6e25c40c43a2927bda5005cc9a186ee3
Greg <88697977+zvxvx@users.noreply.github.com>
2024-09-06 19:45:12 -0700

    Simplify dark-mode

--- src/styleVars.ts
+++ src/styleVars.ts
@@ -1,2 +1,2 @@
 export const STYLE =
-  "prose prose-headings:m-0 dark:text-white bg-zinc-200 dark:bg-zinc-700 dark:prose-strong:text-white prose-headings:dark:text-white prose-hr:border-black dark:prose-hr:border-white prose-a:dark:text-white mx-auto prose-a:no-underline prose-hr:my-2 marker:text-black marker:dark:text-white px-2 prose-ul:p-0 prose-ul:m-0 prose-li:list-none prose-li:p-0";
+  "mx-auto sm:px-0 px-2 prose dark:prose-invert prose-headings:my-2 bg-zinc-300 dark:bg-zinc-800 prose-hr:border-black dark:prose-hr:border-white prose-a:no-underline prose-hr:my-2";
